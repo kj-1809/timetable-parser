fb820488657e300ba881a10a0b77bc1b08d23ac0
Krishna Jindal <88274406+kj-1809@users.noreply.github.com>
2024-08-07 16:54:29 +0530

    fix: update timetable

--- src/main.rs
+++ src/main.rs
@@ -62,12 +62,135 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // YEAR - 2 - B ENC
 
-    let group_sizes: Vec<u32> = vec![4, 4, 4];
+    // let group_sizes: Vec<u32> = vec![4, 4, 4];
+    // let row_start = 6;
+    // let row_end = 145;
+    // let col_start = 142;
+    // let col_end = 165;
+    // let worksheet_name = "2ND YEAR B";
+    // let excel_filename = "dataset.xlsx";
+
+    // year 2 - ece - B
+
+    // let group_sizes: Vec<u32> = vec![5, 5];
+    // let row_start = 6;
+    // let row_end = 145;
+    // let col_start = 112;
+    // let col_end = 131;
+    // let worksheet_name = "2ND YEAR B";
+    // let excel_filename = "dataset.xlsx";
+
+    // year 2 - vlsi - B
+
+    // let group_sizes: Vec<u32> = vec![3];
+    // let row_start = 6;
+    // let row_end = 145;
+    // let col_start = 132;
+    // let col_end = 137;
+    // let worksheet_name = "2ND YEAR B";
+    // let excel_filename = "dataset.xlsx";
+
+    // year 2 - elec instru - A
+
+    let group_sizes: Vec<u32> = vec![4];
     let row_start = 6;
     let row_end = 145;
-    let col_start = 142;
-    let col_end = 165;
-    let worksheet_name = "2ND YEAR B";
+    let col_start = 4;
+    let col_end = 11;
+    let worksheet_name = "2ND YEAR A";
+    let excel_filename = "dataset.xlsx";
+
+    // year 2 - elec eng - A
+
+    // let group_sizes: Vec<u32> = vec![3];
+    // let row_start = 6;
+    // let row_end = 145;
+    // let col_start = 12;
+    // let col_end = 17;
+    // let worksheet_name = "2ND YEAR A";
+    // let excel_filename = "dataset.xlsx";
+
+    // year 2 - elec and comp - A
+    // let group_sizes: Vec<u32> = vec![5];
+    // let row_start = 6;
+    // let row_end = 145;
+    // let col_start = 18;
+    // let col_end = 27;
+    // let worksheet_name = "2ND YEAR A";
+    // let excel_filename = "dataset.xlsx";
+
+    // year 2 - RAI - A
+    // let group_sizes: Vec<u32> = vec![3];
+    // let row_start = 6;
+    // let row_end = 145;
+    // let col_start = 40;
+    // let col_end = 45;
+    // let worksheet_name = "2ND YEAR A";
+    // let excel_filename = "dataset.xlsx";
+
+    // year 2 - mechatronics - A
+    // let group_sizes: Vec<u32> = vec![3];
+    // let row_start = 6;
+    // let row_end = 145;
+    // let col_start = 46;
+    // let col_end = 51;
+    // let worksheet_name = "2ND YEAR A";
+    // let excel_filename = "dataset.xlsx";
+
+    // year 2 - chemical - A
+    // let group_sizes: Vec<u32> = vec![2];
+    // let row_start = 6;
+    // let row_end = 145;
+    // let col_start = 52;
+    // let col_end = 55;
+    // let worksheet_name = "2ND YEAR A";
+    // let excel_filename = "dataset.xlsx";
+
+    // year 2 - bio tech - a
+
+    // let group_sizes: Vec<u32> = vec![5];
+    // let row_start = 6;
+    // let row_end = 145;
+    // let col_start = 56;
+    // let col_end = 65;
+    // let worksheet_name = "2ND YEAR A";
+    // let excel_filename = "dataset.xlsx";
+
+    // year 2 - civil comp - a
+
+    // let group_sizes: Vec<u32> = vec![1];
+    // let row_start = 6;
+    // let row_end = 145;
+    // let col_start = 68;
+    // let col_end = 69;
+    // let worksheet_name = "2ND YEAR A";
+    // let excel_filename = "dataset.xlsx";
+    //
+
+    //year 2 - civil comp - a
+    // let group_sizes: Vec<u32> = vec![2];
+    // let row_start = 6;
+    // let row_end = 145;
+    // let col_start = 70;
+    // let col_end = 73;
+    // let worksheet_name = "2ND YEAR A";
+    // let excel_filename = "dataset.xlsx";
+
+    // copc + coe
+    // let group_sizes: Vec<u32> = vec![8, 5, 5, 5, 5, 5, 5, 6, 6];
+    // let row_start = 6;
+    // let row_end = 145;
+    // let col_start = 4;
+    // let col_end = 103;
+    // let worksheet_name = "2ND YEAR B";
+    // let excel_filename = "dataset.xlsx";
+
+    let group_sizes: Vec<u32> = vec![3];
+    let row_start = 6;
+    let row_end = 145;
+    let col_start = 74;
+    let col_end = 79;
+    let worksheet_name = " 2ND YEAR A";
     let excel_filename = "dataset.xlsx";
 
     let mut classes_data = json_parser::parse_timetable(
